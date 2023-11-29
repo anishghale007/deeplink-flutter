ea33562c8d7d038fcc933d3ff71754380149ab7a
anishghale007 <anishghalegg7@gmail.com>
2023-11-29 14:46:51 +0545

    another fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,18 @@
-import 'package:deeplink/home.dart';
+import 'dart:async';
+
+import 'package:app_links/app_links.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:protocol_handler/protocol_handler.dart';
 
 final router = GoRouter(
   routes: [
     GoRoute(
       path: '/',
-      builder: (_, __) => Scaffold(
-        appBar: AppBar(title: const Text('Home Screen')),
-      ),
+      builder: (_, __) => const MyHomePage(title: 'Flutter Demo Home Page'),
       routes: [
         GoRoute(
           path: 'hello',
-          builder: (_, __) => Scaffold(
-            appBar: AppBar(title: const Text('Details Screen')),
-          ),
+          builder: (_, __) => const MyHomePage(title: 'Second Page'),
         ),
       ],
     ),
@@ -24,127 +21,113 @@ final router = GoRouter(
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await protocolHandler.register('myprotocol');
   runApp(const MyApp());
 }
 
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       // routerConfig: router,
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
-//         useMaterial3: true,
-//       ),
-//       home: const MyHomePage(title: 'Flutter Demo Home Page'),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routerConfig: router,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
+        useMaterial3: true,
+      ),
+      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
 
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({super.key, required this.title});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
 
-//   final String title;
+  final String title;
 
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
 
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
-//   final _navigatorKey = GlobalKey<NavigatorState>();
-//   late AppLinks _appLinks;
-//   StreamSubscription<Uri>? _linkSubscription;
-//   String? link;
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  late AppLinks _appLinks;
+  StreamSubscription<Uri>? _linkSubscription;
+  String? link;
 
-//   void _incrementCounter() {
-//     setState(() {
-//       _counter++;
-//     });
-//   }
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
 
-//   @override
-//   void initState() {
-//     super.initState();
-//     initDeepLinks();
-//   }
+  @override
+  void initState() {
+    super.initState();
+    initDeepLinks();
+  }
 
-//   @override
-//   void dispose() {
-//     _linkSubscription?.cancel();
-//     super.dispose();
-//   }
+  @override
+  void dispose() {
+    _linkSubscription?.cancel();
+    super.dispose();
+  }
 
-//   Future<void> initDeepLinks() async {
-//     _appLinks = AppLinks();
+  Future<void> initDeepLinks() async {
+    _appLinks = AppLinks();
 
-//     // Check initial link if app was in cold state (terminated)
-//     final appLink = await _appLinks.getLatestAppLinkString();
-//     // final appLink = await _appLinks.getInitialAppLink();
-//     if (appLink != null) {
-//       setState(() {
-//         link = appLink.toString();
-//       });
-//       // if (link.toString().contains('/hello')) {
-//       //   if (mounted) {
-//       //     Navigator.of(context).push(MaterialPageRoute(
-//       //       builder: (context) => const SecondScreen(),
-//       //     ));
-//       //   }
-//       // }
-//     }
+    // Check initial link if app was in cold state (terminated)
+    final appLink = await _appLinks.getLatestAppLinkString();
+    // final appLink = await _appLinks.getInitialAppLink();
+    if (appLink != null) {
+      setState(() {
+        link = appLink.toString();
+      });
+    }
 
-//     // Handle link when app is in warm state (front or background)
-//     _linkSubscription = _appLinks.uriLinkStream.listen((uri) {
-//       setState(() {
-//         link = uri.toString();
-//         // uri.removeFragment();
-//       });
-//       // if (uri.toString().contains('/hello')) {
-//       //   Navigator.of(context).push(MaterialPageRoute(
-//       //     builder: (context) => const SecondScreen(),
-//       //   ));
-//       // }
-//     });
-//   }
+    // Handle link when app is in warm state (front or background)
+    _linkSubscription = _appLinks.uriLinkStream.listen((uri) {
+      setState(() {
+        link = uri.toString();
+        // uri.removeFragment();
+      });
+    });
+  }
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             Text(
-//               link ?? 'Empty',
-//               style: Theme.of(context).textTheme.headlineMedium,
-//             ),
-//             const Text(
-//               'You have pushed the button this many times:',
-//             ),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.headlineMedium,
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: const Icon(Icons.add),
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              link ?? 'Empty',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
 
 // class MyApp extends StatefulWidget {
 //   const MyApp({Key? key}) : super(key: key);
@@ -259,24 +242,4 @@ void main() async {
 //   }
 // }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primaryColor: const Color(0xff416ff4),
-        canvasColor: Colors.white,
-        scaffoldBackgroundColor: const Color(0xffF7F9FB),
-        dividerColor: Colors.grey.withOpacity(0.3),
-      ),
-      home: const HomePage(),
-    );
-  }
-}
